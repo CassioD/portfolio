f4ba956ccadaeb4a84c06117846ac1bebfd80b78
CassioD <cassiodans@gmail.com>
2024-02-17 00:09:29 -0300

    adicionado visualizações matplotlib

--- app.py
+++ app.py
@@ -1,4 +1,69 @@
 from flask import Flask, send_file
+import pandas as pd
+import numpy as np  # linear algebra
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Carregar o arquivo CSV
+df = pd.read_csv('./static/kaggle/apple_quality.csv')
+
+df.dropna(inplace=True)  # remover linhas ou colunas de um DataFrame do pandas que contenham valores ausentes
+
+df = df.drop(['A_id'], axis=1)
+
+# Gera o gráfico de coluna sobre a qualidade
+plt.figure(figsize=(7, 5))
+sns.countplot(data=df, x='Quality')
+plt.title('Quality')
+plt.savefig('static/images/quality_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# size vs weight
+plt.figure(figsize=(4, 4), dpi=150)
+sns.scatterplot(data=df, x='Weight', y='Size', alpha=0.5)
+plt.savefig('static/images/size_weight_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# Size vs sweetness
+plt.figure(figsize=(4, 4), dpi=150)
+sns.scatterplot(data=df, x='Sweetness', y='Size', alpha=0.5)
+plt.savefig('static/images/size_sweetness_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# size vs Crunchiness
+plt.figure(figsize=(4, 4), dpi=150)
+sns.scatterplot(data=df, x='Crunchiness', y='Size', alpha=0.5)
+plt.savefig('static/images/size_Crunchiness_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# Size vs Juiciness
+plt.figure(figsize=(4, 4), dpi=150)
+sns.scatterplot(data=df, x='Juiciness', y='Size', alpha=0.5)
+plt.savefig('static/images/size_Juiciness_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# Size vs Ripeness
+plt.figure(figsize=(4, 4), dpi=150)
+sns.scatterplot(data=df, x='Ripeness', y='Size', alpha=0.5)
+plt.savefig('static/images/size_Ripeness_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# Size vs Acidity
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=df, x='Acidity', y='Size')
+plt.savefig('static/images/size_Acidity_plot.png')  # Salvar o gráfico como uma imagem PNG
+plt.close()  # Fechar a figura para liberar memória
+
+# criar variáveis dummy (variáveis binárias) a partir de uma variável categórica no DataFrame
+# Cria 2 colunas: Bad e Good, e atribui o valor 0 ou 1).
+quality_dummies = pd.get_dummies(df['Quality'], dtype='int')
+quality_dummies
+
+# Concatena as variáveis binárias ao database.
+df = pd.concat([df, quality_dummies], axis=1)
+
+# Correlation between features using heatmap
+sns.heatmap(df.corr(), annot=True, cmap='Blues')
 
 app = Flask(__name__)
 
